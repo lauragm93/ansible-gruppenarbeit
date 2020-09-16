6e22113a40f092022eced49942c32e57c4e31b02
SebastianPeterFelixDewald <36820026+SebastianPeterFelixDewald@users.noreply.github.com>
2020-09-16 15:14:14 +0200

    adding docker network

--- docker_network.sh
+++ docker_network.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/zsh
 
-docker create network nginx-loadbalancer
+docker network create nginx-loadbalancer
 
-for (( i=1; i <= 3; i++ ))
-do
-   docker build -t  imagename"$i" .
-done
+#nginx loadbalancer
+docker build -f /home/sebastian/gitrepos/ansible-gruppenarbeit/docker-image-nginx-loadbalancer/Dockerfile -t  nginx-loadbalancer .
+
+#nginx container
+docker build -f /home/sebastian/gitrepos/ansible-gruppenarbeit/docker-image-nginx-website/website1/Dockerfile -t  nginx1 .
+docker build -f /home/sebastian/gitrepos/ansible-gruppenarbeit/docker-image-nginx-website/website2/Dockerfile -t  nginx2 .
+docker build -f /home/sebastian/gitrepos/ansible-gruppenarbeit/docker-image-nginx-website/website3/Dockerfile -t  nginx3 .
+
+#running nginx container with webseite
+docker run --network nginx-loadbalancer --rm --name nginx1-container
+docker run --network nginx-loadbalancer --rm --name nginx2-container
+docker run --network nginx-loadbalancer --rm --name nginx3-container
 
 
-for (( i=1; i <= 3; i++ ))
-do
-   docker run --network nginx-loadbalancer --rm --name containername"$i" imagename
-done
-
-
-docker run --network nginx-loadbalancer  --rm -p 8080:80 image_name
+docker run --network nginx-loadbalancer  --rm -p 8080:80 nginx-loadbalancer
